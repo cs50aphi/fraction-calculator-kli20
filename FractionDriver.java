55d01e026f5df733a3a9bd8a5832ff357bb3e0e0
Kate Li <kli20@punahou.edu>
2020-05-05 02:57:55 +0000

    Final Commit

--- FractionDriver.java
+++ FractionDriver.java
@@ -1,4 +1,4 @@
-// Temp driver for Fraction class
+// Driver for Fraction class
 import java.util.Scanner;
 
 public class FractionDriver {
@@ -9,30 +9,52 @@ public class FractionDriver {
         System.out.println("-------------------------------");
         System.out.println();
 
-        // Keep prompting user while no integer
         Scanner kb = new Scanner(System.in);
-        System.out.print("Please enter an operation (+, -, *, /, = or Q to Quit): ");
-        String operation = getOperation(kb);
-
-
-        System.out.print(operation);
-
-
-        // System.out.print("Den1: "); // denominator
-        // int d1 = kb.nextInt();
-        // System.out.print("Num2: "); // numerator
-        // int n2 = kb.nextInt();
-        // System.out.print("Den2: "); // denominator
-        // int d2 = kb.nextInt();
-        // Fraction f1 = new Fraction(n1, d1);
-        // Fraction f2 = new Fraction(n2, d2);
-        // System.out.println(f1 + " + " + f2 + " is " + f1.add(f2));
-        // System.out.println(f1 + " - " + f2 + " is " + f1.subtract(f2));
-        // System.out.println(f1 + " * " + f2 + " is " + f1.multiply(f2));
-        // System.out.println(f1 + " / " + f2 + " is " + f1.divide(f2));
-        // System.out.println(f1 + " is the same as " + f2 + ": " + f1.equals(f2));
+        // Initialize done to false
+        boolean done = false;
+        while (!done) {
+            System.out.print("Please enter an operation (+, -, *, /, = or Q to Quit): ");
+            String operation = getOperation(kb);
+            // If user quits, exit
+            if (operation.equals("Q") || operation.equals("q")) {
+                done = true;
+                break;
+            }
+            // Get fractions from user
+            System.out.print("Please enter a fraction (a/b) or integer (a): ");
+            Fraction f1 = getFraction(kb);
+            System.out.print("Please enter a fraction (a/b) or integer (a): ");
+            Fraction f2 = getFraction(kb);
+            
+            // Assign operations to the corresponding methods
+            switch (operation) {
+                case "+":
+                    Fraction addition = f1.add(f2);
+                    addition.toLowestTerms();
+                    System.out.println(f1 + " + " + f2 + " is " + addition);
+                    break;
+                case "-":
+                    Fraction subtraction = f1.subtract(f2);
+                    subtraction.toLowestTerms();
+                    System.out.println(f1 + " - " + f2 + " is " + subtraction);
+                    break;
+                case "*":
+                    Fraction multiplication = f1.multiply(f2);
+                    multiplication.toLowestTerms();
+                    System.out.println(f1 + " * " + f2 + " is " + multiplication);
+                    break;
+                case "/":
+                    Fraction division = f1.divide(f2);
+                    division.toLowestTerms();
+                    System.out.println(f1 + " / " + f2 + " is " + division);
+                    break;
+                case "=":
+                    System.out.println(f1 + " is the same as " + f2 + ": " + f1.equals(f2));
+                    break;
+            }
+        }
     }
-    // Keep prompting user while there is no operation selected (else)
+    // Keep prompting user for operation while there is no operation selected (else)
     public static String getOperation(Scanner kb) {
         String input = kb.nextLine();
         // while invalid operation, print invalid
@@ -41,5 +63,69 @@ public class FractionDriver {
             input = kb.nextLine();
         }
         return input;
+    }
+    // Returns if fraction is valid or not
+    public static boolean validFraction(String input) {
+        // negative sign can only be at front (remove it if there is one)
+        // if there is neg sign at 0, remove it
+        if (input.indexOf("-") == 0)
+        {
+            input = input.substring(1);
+        }
+        // if there is neg sign anywhere else, return false (not a valid frac)
+        else if (input.indexOf("-") > 0) {
+            return false;
+        }
+        // If there is no "/" character, then every character in the string must be a number
+        if (input.indexOf("/") == -1) {
+            // if every char is a number, return true;
+            if (input.matches("[0-9]+")) {
+                return true;
+            }
+            return false;
+        }
+        // If there is a "/" character, create substrings for the numerator and denominator.
+        if (input.indexOf("/") >= 0) {
+            // Isolate numerator
+            String numTemp = input.substring(0, input.indexOf("/"));
+            // Isolate denominator
+            String denTemp = input.substring(input.indexOf("/") + 1);
+            // Both substrings must be non-empty.
+            if (numTemp.equals("") || denTemp.equals("")){
+                return false;
+            }
+            // Both must be entirely made of numbers.
+            if (numTemp.matches("[0-9]+") && denTemp.matches("[0-9]+")) {
+                // The denominator cannot be "0".
+                if (!denTemp.equals("0")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    // Prompts user for valid fractions and returns Fraction
+    public static Fraction getFraction(Scanner kb) {
+        String frac = kb.nextLine();
+        // while fraction is not valid, keep prompting and print invalid
+        while (!validFraction(frac)) {
+            System.out.print("Invalid input. Please enter a fraction (a/b) or integer (a): ");
+            frac = kb.nextLine();
+        }
+        int num = 0;
+        int den = 0;
+        // If there is a "/" character, create substrings for the numerator and denominator.
+        if (frac.indexOf("/") >= 0) {
+            // Isolate numerator
+            num = Integer.parseInt(frac.substring(0, frac.indexOf("/")));
+            // Isolate denominator
+            den = Integer.parseInt(frac.substring(frac.indexOf("/") + 1));
+        }
+        // If there is NO "/" character
+        if (frac.indexOf("/") == -1) {
+            num = Integer.parseInt(frac);
+            den = 1;
+        }
+        return new Fraction(num, den);
     }
 }
